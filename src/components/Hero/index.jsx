b0d605b97ba7b0c23bd773f4f77e5168d9c5de54
Alejandro Cardozo <uri.cardozo@gmail.com>
2022-02-14 16:45:53 -0300

    Hero section component

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import Video from '../../videos/video.mp4';
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroBtnWrapper,
+  HeroContent,
+  HeroH1,
+  HeroP,
+} from './HeroElements';
+
+const Hero = () => {
+  const [hover, setHover] = useState(false)
+
+  const onHoverHandler = () => {
+    setHover(prev => !prev)
+  }
+
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+        <HeroP>
+          Sign up for a new account today and receive $250 in credit towards
+          your next payment
+        </HeroP>
+        <HeroBtnWrapper>
+          {/* <Button to='signup' onMouseEnter={onHoverHandler} onMouseLeave={onHoverHandler}>
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button> */}
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default Hero;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Hero from '../components/Hero';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 
@@ -13,6 +14,7 @@ const Home = () => {
     <>
       <Sidebar isOpen={isOpen} toggle={toggleMenuHandler} />
       <Navbar toggle={toggleMenuHandler} />
+      <Hero />
     </>
   );
 };
